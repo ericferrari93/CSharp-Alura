9118aedd7737335ab3b9e1acbc30a96f5663df9f
Eric Ferrari Ramos <ericferrarir1@gmail.com>
2022-08-22 16:52:21 -0300

    Primeiro Commit introdução ao C#

---
+++ AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Criando Variáveis ponto flutuante");
+
+        double salario;
+        salario = 3000.10;
+
+        salario = 3000;
+
+        double idade;
+
+        idade = 7.0 / 5;
+
+        Console.WriteLine(idade);
+
+        
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
---
+++ AprendendoCSharp/4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 4 - Conversões e outros tipos");
+
+        double salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+        // O long é um tipo de variavel de 64 bits.
+        long x = 200000000000000000;
+        Console.WriteLine(x);
+
+        short y = 1500;
+        Console.WriteLine(y);
+
+        float altura = 1.63f;
+        Console.WriteLine(altura);
+
+
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
